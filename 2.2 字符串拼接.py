7e76f997f56a56176f82d8c9ed02402539fe2270
cnseu <sec1937@163.com>
2015-09-01 19:50:00 +0800

    Revert "OMG"
    This reverts commit fa6974888738971d5b124c85324c4ece5c70c771.

--- 2.2 字符串拼接.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 05 21:53:26 2015
-
-@author: Administrator
-"""
-
-print(str(0) + '*' + str(0) + ' = ' +str(9))
-结果是0*0=9
--- 2.6.1 who cool.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 09 20:39:41 2015
-
-@author: Administrator
-"""
-
-for cool_guy in ["A","B","c","d"]:
-    print cool_guy,"is the coolest guy ever!"
